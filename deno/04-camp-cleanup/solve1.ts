20b4da8ab217eed634f94cbc4255bbfe4f56fdc6
Federico Grandi <federicograndi@duck.com>
2022-12-04 11:25:02 +0100

    feat(deno): add day 4

---
+++ deno/04-camp-cleanup/solve1.ts
@@ -0,0 +1,21 @@
+import {
+  readFileSync,
+  writeFileSync,
+} from "https://deno.land/std@0.167.0/node/fs.ts";
+
+const input = readFileSync("./input.txt", "utf8")
+  .trim()
+  .split("\n")
+  .map((pair) => pair.split(",").map((range) => range.split("-").map(Number)));
+
+const output = input.reduce(
+  (acc, [r1, r2]) =>
+    acc +
+    Number(
+      (r1[0] >= r2[0] && r1[1] <= r2[1]) || (r2[0] >= r1[0] && r2[1] <= r1[1])
+    ),
+  0
+);
+
+console.log(output);
+writeFileSync("./output1.txt", output.toString());
